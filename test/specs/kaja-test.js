9b1159fc65fa63cf49e3b2bfc7621547f4250362
Kaja Nowakova <kaja.nowakova@gmail.com>
2021-05-18 19:27:53 +0200

    h

--- test/specs/kaja-test.js
+++ test/specs/kaja-test.js
@@ -3,5 +3,7 @@ const templatePage = require('../pages/template.page')
 describe('Template suite', () => {
     it('template test-case', () => {
         sdfasdfasdfasfasfdsfasfdasdfa 
+
+        jkjhk
     })
 })
